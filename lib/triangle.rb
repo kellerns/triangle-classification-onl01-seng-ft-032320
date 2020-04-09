a2bcab0b9c2e385e7014e6c7e892afc26b3ca32c
Nicholas Keller <nic.keller328@gmail.com>
2020-04-09 16:51:07 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -5,8 +5,6 @@ class Triangle
     if (side_1 <= 0 || side_2 <= 0 || side_3 <= 0) || ((side_1 + side_2) <= side_3 || (side_1 + side_3) <= side_2 || (side_2 + side_3) <= side_1)
       begin
         raise TriangleError
-      rescue TriangleError => error
-          puts error
       end
     else
       @side_1 = side_1
