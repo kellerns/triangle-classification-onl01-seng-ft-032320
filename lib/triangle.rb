bb98f456071fd8f5d7dec9826d9976a7ca3718e3
Nicholas Keller <nic.keller328@gmail.com>
2020-04-09 16:49:10 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -2,7 +2,7 @@ class Triangle
   attr_accessor :side_1, :side_2, :side_3
 
   def initialize(side_1, side_2, side_3)
-    if (side_1 <= 0 || side_2 <= 0 || side_3 <= 0) || ((side_1 + side_2) > side_3 || (side_1 + side_3) > side_2 || (side_2 + side_3) > side_1)
+    if (side_1 <= 0 || side_2 <= 0 || side_3 <= 0) || ((side_1 + side_2) !> side_3 || (side_1 + side_3) !> side_2 || (side_2 + side_3) !> side_1)
       begin
         raise TriangleError
       rescue TriangleError => error
